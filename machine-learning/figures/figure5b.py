2644805e2b9bb11f0338b156e5ec93a399c9b8ad
Logan Ward <ward.logan.t@gmail.com>
2017-06-26 11:57:08 -0500

    Added ML figures into the repository
    Needs some work to merge this with the scripts in
    "/scripts/figure_plotters/"
    
    I could not get those scripts to run (I think it is a version
    incompatibility with how packages work). So, I created my own separate
    directory as to not interfere with Fang and Travis's scripts.

---
+++ machine-learning/figures/figure5b.py
@@ -0,0 +1,67 @@
+# -*- coding: utf-8 -*-
+"""
+Plot the formation enthalpy predicted with 
+    - Trained on meltspinning and sputtering GFA
+    - Accounts for processing with "stacked" approach
+    - Uses Yang and Laws theories
+    
+@author: fangren
+"""
+
+# Load in libraries
+import numpy as np
+import matplotlib.pyplot as plt
+from matplotlib import cm
+from scipy import interpolate
+import imp
+import os
+import pandas as pd
+from ternary_helper import interpolation_ternary
+
+# Important variables to change
+path = os.path.join('..','3_with-CoVZr-data','plots')
+save_path = os.path.join('..','..','figures')
+
+# Load in the ML results
+filename_new = os.path.join(path, 'FeNbTi.csv')
+data = pd.read_csv(filename_new)
+
+Fe = data['X_Fe']*100
+Nb = data['X_Nb']*100
+Ti = data['X_Ti']*100
+probability = data['probability']
+
+
+# Interpolate probabilities
+probability_func = interpolate.Rbf(Fe, Nb, Ti, probability, function='multiquadric',
+                                       smooth=0.3)
+
+metal1_range = np.arange(0, 100, 1.25)
+metal2_range = np.arange(0, 100, 1.25)
+
+metal1 = []
+metal2 = []
+metal3 = []
+
+probability_interpolate = []
+
+for i in metal1_range:
+    for j in metal2_range:
+        if i + j <= 100 and i+j >= 0:
+            try:
+                metal1.append(i)
+                metal2.append(j)
+                metal3.append(100-i-j)
+                probability_interpolate.append(float(probability_func(i, j, (100-i-j))))
+            except(NbalueError):
+                continue
+
+
+# Make a new colormap that has alpha of 0.5 below 0.95%
+                
+ternary_data = np.concatenate(([metal1],[metal2],[metal3],[probability_interpolate]), axis = 0)
+ternary_data = np.transpose(ternary_data)
+
+interpolation_ternary(ternary_data, tertitle='',  labelNames=('Fe', 'Nb', 'Ti'), scale=100,
+                       sv=True, svpth=save_path, svflnm='Figure5b.png',
+                       cbl='Liklihood (GFA = True)', vmin = 0.5, vmax = 1, cmap='viridis_r', cb=True, style='h')
---
+++ machine-learning/figures/ternary_helper.py
@@ -0,0 +1,98 @@
+__author__ = "Travis Williams"
+# University of South Carolina
+# Jason Hattrick-Simpers group
+# Starting Date: June, 2016
+
+import matplotlib.pyplot as plt
+import numpy as np
+import os
+
+import ternary
+
+def interpolation_ternary(data, tertitle='',  labelNames=('Species A','Species B','Species C'), scale=100,
+                       sv=False, svpth=r"C:/Users/Travis W/Pictures/", svflnm='Unnamed',
+                       cbl='Scale', vmin=None, vmax=None, cmap='viridis', cb=True, style='h'):
+    """
+    Overview
+    ----------
+    This program makes use of the ternary package and creates a ternary colormap
+
+    Parameters
+    ----------
+    data: 4-tup of (a,b,c,i) where a, b, and c are the ternary coordinates and i is the intensity
+    tertitle: chart title
+    labelNames: Names of a, b, and c for chart axes
+    scale: scale of chart (if 0-100, then scale is 100)
+    sv: Boolean, save chart when true, show chart when false
+    svpth: path to save ternary image
+    svflnm: file name for ternary diagram when saving
+    cbl: colorbar label
+    vmin: minimum value of colorbar (leave blank to set to min value in i)
+    vmax: maximum value of colorbar (leave blank to set to max value in i)
+    cmap: determines color map used
+    cb: use colorbar if true
+    style: h = hexagons, d = triangles for point shape
+
+    Returns
+    -------
+
+    """
+    if sv:
+        font = {'size': 12}   # Set font size for **kwargs
+        figsize = (3.4, 2.5)
+        ticksize = 6
+        lnwdth = 0.5
+        lnsty = '--'
+        alpha = 0.15
+    else:
+        font = {'size': 30}   # Set font size for **kwargs
+        figsize = (30, 30)
+        ticksize = 20
+        lnwdth = 2
+        lnsty = ':'
+        alpha = 0.5
+
+    d = dict()
+    x = data[:, 1]
+    y = data[:, 0]
+    if cb:
+        i = data[:, 3]
+
+        for col in range(1, len(x)):
+            d[(x[col], y[col])] = i[col]
+    else:
+        for col in range(1, len(x)):
+            d[(x[col], y[col])] = 0.5
+
+    # Turn off normal axis, set figure size
+    figure, ax = plt.subplots(figsize=figsize)
+    ax.axis("off")
+
+    # Create ternary axes (tax)
+    figure, tax = ternary.figure(ax=ax, scale=scale)
+
+    # Axis Labels (bottom corrisponds to x values, left corrisponds to y values)
+    tax.bottom_axis_label(labelNames[1], offset=-0.1, **font)
+    tax.left_axis_label(labelNames[2], offset=0.17, **font)
+    tax.right_axis_label(labelNames[0], offset=0.17, **font)
+
+    # Plot data, boundary, gridlines, and ticks
+    tax.heatmap(d, style=style, cmap=cmap, cbarlabel=cbl, vmin=vmin, vmax=vmax, colorbar=cb)
+    tax.boundary(linewidth=1)
+    tax.gridlines(multiple=10, linewidth=lnwdth, alpha=alpha, linestyle=lnsty)
+    ticks = [round(i / float(scale), 1) for i in range(0, scale+1, 10)]
+    tax.ticks(ticks=ticks, axis='rlb', linewidth=1, clockwise=False, offset=0.03, fsize=ticksize)
+
+    # Set chart title
+    tax.set_title(tertitle)
+
+    # Make chart pretty
+    tax.clear_matplotlib_ticks()
+    tax._redraw_labels()
+    plt.tight_layout()
+
+    # Save or show
+    if sv:
+        plt.savefig(os.path.join(svpth, svflnm), dpi=600)
+    else:
+        tax.show()
