c877b0538b451bd50cf0cdd385f4d9b7e9878f61
Logan Ward <ward.logan.t@gmail.com>
2017-10-17 19:18:40 -0500

    Fixed Figure5a, should be CoVZr

--- machine-learning/figures/figure5a.py
+++ machine-learning/figures/figure5a.py
@@ -28,12 +28,12 @@ path = os.path.join('..','4_with-CoVZr-data','plots')
 save_path = os.path.join('..','..','figures')
 
 # Load in the ML results
-filename_new = os.path.join(path, 'FeNbTi.csv')
+filename_new = os.path.join(path, 'CoVZr.csv')
 data = pd.read_csv(filename_new)
 
-Fe = data['X_Fe']*100
-Nb = data['X_Nb']*100
-Ti = data['X_Ti']*100
+Co = data['X_Co']*100
+V = data['X_V']*100
+Zr = data['X_Zr']*100
 probability = data['probability']
 
 # Load in the log loss
@@ -41,12 +41,12 @@ with open(os.path.join('..','4_with-CoVZr-data','run-HiTp-data.out')) as fp:
     # Get the lines with "log-loss" data
     data_lines = [ x for x in fp if 'Log-loss' in x ]
 
-    # The first is CoVZr, second is FeNbTi
-    log_loss = float(data_lines[1].split()[-1])
+    # The first is CoVZr, second is CoVTi
+    log_loss = float(data_lines[0].split()[-1])
 
 # Interpolate probabilities
-ternary_data = interpolate_probabilities(Fe, Ti, Nb, probability)
-interpolation_ternary(ternary_data, tertitle='',  labelNames=('Fe', 'Ti', 'Nb'), scale=100,
+ternary_data = interpolate_probabilities(Co, V, Zr, probability)
+interpolation_ternary(ternary_data, tertitle='',  labelNames=('Co', 'V', 'Zr'), scale=100,
                        sv=True, svpth=save_path, svflnm='/Figure5_CoVZr-gen2.png',
                        cbl='Likelihood (GFA = True)', vmin = 0.5, vmax = 1, cmap=make_cmap(), cb=True, style='h',
                        other_labels=[(50, -17.5, 'Log-loss: %.3f'%log_loss)])
